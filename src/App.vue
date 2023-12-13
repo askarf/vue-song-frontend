ca35d852d420b48fbd3a153e8841a262e4536918
Jack <jconnolly444@gmail.com>
2023-12-13 14:23:57 -0600

    create song action

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ export default {
   data: function () {
     return {
       songs: [],
+      newSongParams: {},
     };
   },
   created: function () {
@@ -16,12 +17,45 @@ export default {
         this.songs = response.data;
       });
     },
+    createSong: function () {
+      const formData = new FormData();
+      formData.append("title", this.newSongParams.title);
+      formData.append("artist", this.newSongParams.artist);
+      formData.append("album", this.newSongParams.album);
+      formData.append("duration", this.newSongParams.duration);
+      axios
+        .post("http://localhost:5000/songs.json", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((response) => {
+          console.log("songs create", response);
+          this.songs.push(response.data);
+          this.newSongParams = {};
+        })
+        .catch((error) => {
+          console.log("songs create error", error.response);
+        });
+    },
   },
 };
 </script>
 
 <template>
   <div class="home">
+    <h1>New Song</h1>
+    <div>
+      Title:
+      <input type="text" v-model="newSongParams.title" />
+      Artist:
+      <input type="text" v-model="newSongParams.artist" />
+      Album:
+      <input type="text" v-model="newSongParams.album" />
+      Duration:
+      <input type="text" v-model="newSongParams.duration" />
+      <button v-on:click="createSong()">Create Song</button>
+    </div>
     <h1>All Songs</h1>
     <div v-for="song in songs" v-bind:key="song.id">
       <h2>{{ song.title }}</h2>
