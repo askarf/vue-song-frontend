8295ca4ad92cce64cc1e675cac8899108d38d939
stevenungaro <144843553+stevenungaro@users.noreply.github.com>
2023-12-14 14:01:47 -0600

    split sogns and artist onto two pages

--- src/main.js
+++ src/main.js
@@ -2,7 +2,23 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import axios from "axios";
+import { createRouter, createWebHistory } from "vue-router";
+import Songs from "./components/Songs.vue";
+import Artists from "./components/Artists.vue";
+
+const routes = [
+  { path: "/songs", component: Songs },
+  { path: "/artists", component: Artists },
+  // Other routes if needed
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
-createApp(App).mount("#app");
+createApp(App).use(router).mount("#app");
